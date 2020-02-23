10d3098cc6357ccc0813033f54983fd346bc9a3f
tessaSAC <tessaSAC@users.noreply.github.com>
2020-02-23 18:03:00 -0500

    starting point

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h2>Please add your code here.</h2>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <li><a href="https://github.com/axios/axios" target="_blank">axios</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 200px;
+}
+
+h1,
+h2 {
+  font-weight: normal;
+}
+
+img {
+  width: 200px;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #6a78d1;
+}
+</style>
